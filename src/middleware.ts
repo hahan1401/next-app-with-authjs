0404b0f63e81f1737f169e8df724f617d78e1999
hahan <hanvietha141@gmail.com>
2024-06-01 11:15:44 +0700

    (+) implement middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,23 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export const middleware = auth((req) => {
+	const requestHeaders = new Headers(req.headers);
+  console.log('~~~~~~~~~~~~~~~~~~~~', req.auth);
+	try {
+		return NextResponse.next({
+			request: {
+				headers: requestHeaders
+			}
+		});
+    } catch (err) {
+		return NextResponse.redirect(new URL('/', req.url));
+	}
+});
+
+export const config = {
+	matcher: [
+		'/',
+		'/((?!api|_next/static|_next/image|favicon.ico).*)',
+	]
+};
